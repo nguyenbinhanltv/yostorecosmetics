5308f361e4ffe19129208a5fd341a7989983c7d4
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-06-20 00:20:29 +0700

    init project

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  declarations: [
+    SidebarComponent
+  ],
+  exports: [
+    SidebarComponent
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  { path: '/dashboard', title: 'Danh sách sản phẩm', icon: 'dashboard', class: '' },
+  { path: '/user-profile', title: 'Khách hàng', icon: 'person', class: '' },
+  {
+    path: '/table-list',
+    title: 'Quản lý kho',
+    icon: 'house_siding',
+    class: '',
+  },
+  {
+    path: '/typography',
+    title: 'Nhập hàng',
+    icon: 'add_shopping_cart',
+    class: '',
+  },
+  { path: '/icons', title: 'Kiểm hàng', icon: 'shopping_cart', class: '' },
+  { path: '/maps', title: 'Chuyển hàng', icon: 'remove_shopping_cart', class: '' },
+  {
+    path: '/notifications',
+    title: 'Điều chỉnh giá vốn',
+    icon: 'monetization_on',
+    class: '',
+  },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter((menuItem) => menuItem);
+  }
+  isMobileMenu() {
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
+  }
+}
