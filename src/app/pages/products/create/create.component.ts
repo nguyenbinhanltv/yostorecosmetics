3be5e5ff10c43dd8d71af1d4c400a33f32214199
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-06-24 15:43:46 +0700

    update products page

--- src/app/pages/products/create/create.component.ts
+++ src/app/pages/products/create/create.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import {
+  Validators,
+  FormControl,
+  FormGroup,
+  FormBuilder,
+} from '@angular/forms';
 import { SelectItem } from 'primeng/api';
 import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent implements OnInit {
-  userform: FormGroup;
+  productFrom: FormGroup;
 
   submitted: boolean;
 
@@ -17,20 +22,33 @@ export class CreateComponent implements OnInit {
 
   description: string;
 
-  constructor(private fb: FormBuilder, private messageService: MessageService) {}
+  constructor(
+    private fb: FormBuilder,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
-      this.userform = this.fb.group({
-          'firstname': new FormControl('', Validators.required),
-          'lastname': new FormControl('', Validators.required),
-          'password': new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
-          'description': new FormControl('')
-      });
+    this.productFrom = this.fb.group({
+      productname: new FormControl('', Validators.required),
+      productid: new FormControl('', Validators.required),
+      barcode: new FormControl('', Validators.required),
+      productretailprices: new FormControl('', Validators.required),
+      productwholesaleprices: new FormControl('', Validators.required),
+      productentryprices: new FormControl('', Validators.required),
+      mass: new FormControl('', Validators.required),
+      unit: new FormControl('', Validators.required),
+      producttype: new FormControl('', Validators.required),
+      brand: new FormControl('', Validators.required),
+    });
+
   }
 
   onSubmit(value: string) {
-      this.submitted = true;
-      this.messageService.add({severity: 'info', summary: 'Success', detail: 'Form Submitted'});
+    this.submitted = true;
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'Form Submitted',
+    });
   }
-
 }
