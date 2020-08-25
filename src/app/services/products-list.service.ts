8b3599de423ccf6a93c8175be178531fdd6e5a02
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-26 00:46:57 +0700

    update products service

--- src/app/services/products-list.service.ts
+++ src/app/services/products-list.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'environments/environment';
 import { Product } from 'app/models/products.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,19 +13,19 @@ export class ProductsListService {
     public http: HttpClient
   ) { }
 
-  getProducts() {
+  getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(environment.apiEndPoint + '/products');
   }
 
-  createProduct(product: Product) {
+  createProduct(product: Product): Observable<Product> {
     return this.http.post<Product>(environment.apiEndPoint + '/products', product);
   }
 
-  updateProduct(productId: string, product: Product) {
+  updateProduct(productId: string, product: Product): Observable<Product> {
     return this.http.patch<Product>(environment.apiEndPoint + `/products/${productId}`, product);
   }
 
-  deleteProduct(productId: string) {
+  deleteProduct(productId: string): Observable<Product> {
     return this.http.delete<Product>(environment.apiEndPoint + `/products/${productId}`);
   }
 }
