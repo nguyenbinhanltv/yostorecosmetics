5c395169304dcebfffee493cdea03c4d351af43a
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-26 02:03:22 +0700

    update options service

--- src/app/services/options.service.ts
+++ src/app/services/options.service.ts
@@ -11,4 +11,24 @@ export class OptionsService {
   constructor(
     private http: HttpClient
   ) { }
+
+  getOptions(): Observable<any> {
+    return this.http.get<any>(environment.apiEndPoint + '/options');
+  }
+
+  getOption(optionId: string): Observable<any> {
+    return this.http.get<any>(environment.apiEndPoint + `/options/${optionId}`);
+  }
+
+  createOption(option): Observable<any> {
+    return this.http.post<any>(environment.apiEndPoint + '/options', option);
+  }
+
+  updateOption(optionId: string, option): Observable<any> {
+    return this.http.patch<any>(environment.apiEndPoint + `/options/${optionId}`, option);
+  }
+
+  deleteProduct(optionId: string): Observable<any> {
+    return this.http.delete<any>(environment.apiEndPoint + `/options/${optionId}`);
+  }
 }
