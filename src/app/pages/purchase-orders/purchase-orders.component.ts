ed848fc862124c73dad1cb96073e49bd799a48af
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-29 01:16:29 +0700

    update purchaseOrders page

--- src/app/pages/purchase-orders/purchase-orders.component.ts
+++ src/app/pages/purchase-orders/purchase-orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { of, forkJoin } from 'rxjs';
 import { delay } from 'rxjs/operators';
+import { PurchaseOrdersService } from 'app/services/purchase-orders.service';
 
 @Component({
   selector: 'app-purchase-orders',
@@ -8,15 +9,19 @@ import { delay } from 'rxjs/operators';
   styleUrls: ['./purchase-orders.component.css'],
 })
 export class PurchaseOrdersComponent implements OnInit {
-  cars = [];
+  purchaseOrders = [];
 
   isLoading: boolean;
 
-  constructor() {
+  constructor(
+    private purchaseOrdersService: PurchaseOrdersService
+  ) {
     forkJoin(
       of(true).pipe(delay(1000)),
+      this.purchaseOrdersService.getAllPurchaseOrders()
     ).subscribe(data => {
       this.isLoading = data[0];
+      this.purchaseOrders = data[1];
     });
   }
 
