bcd9f8f9246912b63b391b6eaffd5fe120b1e777
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-30 17:13:18 +0700

    update purchase-orders page

--- src/app/pages/purchase-orders/purchase-orders.component.ts
+++ src/app/pages/purchase-orders/purchase-orders.component.ts
@@ -3,6 +3,10 @@ import { of, forkJoin } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { PurchaseOrdersService } from 'app/services/purchase-orders.service';
 import { Product } from 'app/models/products.model';
+import { PurchaseOrders } from 'app/models/purchase-orders.model';
+
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-purchase-orders',
@@ -10,16 +14,28 @@ import { Product } from 'app/models/products.model';
   styleUrls: ['./purchase-orders.component.css'],
 })
 export class PurchaseOrdersComponent implements OnInit {
-  purchaseOrders = [];
+  purchaseOrders: PurchaseOrders[];
 
   isLoading: boolean;
 
+  // Products dialog
   productDialog: boolean;
-  submitted: boolean;
   products: Product[];
 
+  // PurchaseOrders dialog
+  purchaseOrdersDialog: boolean;
+  submitted: boolean;
+  purchaseOrder: any;
+
+  // Options
+  purchaseOrdersStatusOptions;
+  purchaseOrdersPaymentStatusOptions;
+
+
   constructor(
-    private purchaseOrdersService: PurchaseOrdersService
+    private purchaseOrdersService: PurchaseOrdersService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
   ) {
     forkJoin(
       of(true).pipe(delay(1000)),
@@ -32,6 +48,15 @@ export class PurchaseOrdersComponent implements OnInit {
 
   ngOnInit(): void {
     of(false).subscribe(val => this.isLoading = val);
+
+    this.purchaseOrdersStatusOptions = [
+      { name: 'Waiting' },
+      { name: 'Recieved' },
+    ];
+    this.purchaseOrdersPaymentStatusOptions = [
+      { name: 'Paid' },
+      { name: 'Unpaid' },
+    ];
   }
 
   editProducts(products: Product[]): void {
@@ -40,8 +65,58 @@ export class PurchaseOrdersComponent implements OnInit {
   }
 
   hideDialog(): void {
-    this.productDialog = false;
+    this.purchaseOrdersDialog = false;
     this.submitted = false;
+  }
+
+  deletePurchaseOrders(purchaseOrders: PurchaseOrders): void {
+    this.confirmationService.confirm({
+      message: 'Bạn chắc có muốn xóa đơn hàng ' + purchaseOrders.purchaseOrdersId + '?',
+      header: 'Cảnh báo',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.purchaseOrdersService.deletePurchaseOrders(purchaseOrders.purchaseOrdersId).subscribe();
+        this.messageService.add({
+          severity: 'success',
+          summary: 'OK',
+          detail: 'Đã xóa đơn hàng',
+          life: 3000,
+        });
+      }
+    });
+
+    this.purchaseOrder = null;
+  }
+
+  editPurchaseOrders(purchaseOrders: PurchaseOrders): void {
+    this.purchaseOrder = {
+      purchaseOrdersId: purchaseOrders.purchaseOrdersId,
+      purchaseOrdersStatus: [{
+        'name': purchaseOrders.purchaseOrdersStatus
+      }],
+      purchaseOrdersPaymentStatus: [{
+        'name': purchaseOrders.purchaseOrdersPaymentStatus
+      }],
+    };
+    console.log(this.purchaseOrder);
+    this.purchaseOrdersDialog = true;
+  }
+
+  savePurchaseOrders(pruchaseOrders: PurchaseOrders): void {
+    this.submitted = true;
+
+    if (pruchaseOrders.purchaseOrdersId) {
+      this.purchaseOrdersService.updatePurchaseOrders(pruchaseOrders.purchaseOrdersId, pruchaseOrders).subscribe();
+    }
+
+    this.messageService.add({
+      severity: 'success',
+      summary: 'OK',
+      detail: 'Cật nhật đơn hàng thành công.',
+      life: 3000,
+    });
+    this.purchaseOrdersDialog = false;
+    this.purchaseOrder = null;
   }
 
 }
